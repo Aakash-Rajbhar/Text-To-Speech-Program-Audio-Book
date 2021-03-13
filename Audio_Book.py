3c952c9452ce8274afa3015b0b3be88760f62ec8
Aakash Rajbhar <73937206+Aakash-Rajbhar@users.noreply.github.com>
2021-03-13 19:56:23 +0530

    Update Audio_Book.py

--- Audio_Book.py
+++ Audio_Book.py
@@ -4,9 +4,9 @@ from gtts import gTTS
 #Import os module
 import os
 
-mytext='''Hello Aakash'''
+mytext='''Hello Aakash'''      #The text you want to covert into speech
 
-language='en'
+language='en'                   #Selecting the language
 myobj = gTTS(text=mytext,lang=language, slow=False)
 myobj.save("audio.mp3")
 os.system("audio.mp3")
